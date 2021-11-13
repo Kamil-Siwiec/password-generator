4179004a95de7540c7f4c1d949339234b9d0ba4f
Kamil Siwiec <kamsiwcode@gmail.com>
2021-11-13 15:01:09 +0100

    add js, get dom elements

--- js/main.js
+++ js/main.js
@@ -6,3 +6,8 @@ const lowerEl = document.getElementById("lower");
 const numberEl = document.getElementById("number");
 const symbolEl = document.getElementById("symbol");
 const generateEl = document.getElementById("generate");
+
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+=";
